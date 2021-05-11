8684caa31677ec812cec6c1b5752a10fb87251a3
Sari27 <48992399+Sari27@users.noreply.github.com>
2021-05-11 09:47:40 +0200

    Création du projet

---
+++ DI3ProjetJava/src/Controller/Main.java
@@ -0,0 +1,7 @@
+package Controller;
+
+public class Main {
+	public static void main(String[] args){
+		System.out.println("Helllo from Main controller\n");
+	}
+}
---
+++ DI3ProjetJava/src/Model/Main.java
@@ -0,0 +1,7 @@
+package Model;
+
+public class Main {
+	public static void main(String[] args){
+		System.out.println("Helllo from Main model\n");
+	}
+}
