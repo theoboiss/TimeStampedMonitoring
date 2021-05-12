3f4af2203ea78dd1d1544ab99cc86d907372ceac
theo-boi <48034060+theo-boi@users.noreply.github.com>
2021-05-12 14:59:15 +0200

    Test src/Controller/main.java (théo)

--- DI3ProjetJava/src/Controller/Main.java
+++ DI3ProjetJava/src/Controller/Main.java
@@ -2,6 +2,6 @@ package Controller;
 
 public class Main {
 	public static void main(String[] args){
-		System.out.println("Helllo from Main controller\n");
+		System.out.println("Hello from Main controller\n");
 	}
 }
