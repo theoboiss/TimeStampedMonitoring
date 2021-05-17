fa12ec286d98644ac26d8f000391f6414d4f15e5
theo-boi <48034060+theo-boi@users.noreply.github.com>
2021-05-17 20:18:56 +0200

    Correction organisation : Employee déplacé vers model.mainapp

--- DI3ProjetJava/src/model/mainapp/Employee.java
+++ DI3ProjetJava/src/model/mainapp/Employee.java
@@ -1,4 +1,4 @@
-package view.mainapp;
+package model.mainapp;
 
 import java.util.Arrays;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -135,5 +135,22 @@ public class Employee {
 	public String toString() {
 		return "Employee [ID=" + getID() + ", firstName=" + getFirstName() + ", lastName=" + getLastName() + ", listChecks="
 				+ Arrays.toString(getListChecks()) + "]";
+	}
+	
+	
+	public static void main(String[] args) {
+		CopyOnWriteArrayList<Employee> listEmployees = new CopyOnWriteArrayList<>();
+		
+		listEmployees.add(new Employee("Barney", "Stinson"));
+		listEmployees.add(new Employee("Ted", "Mosby"));
+		listEmployees.add(new Employee("Rachel", "Green"));
+		listEmployees.add(new Employee("Joey", "Tribiani"));
+		listEmployees.add(new Employee("Jesse", "Pinkman"));
+
+		System.out.println("UsedIDs ="+Employee.getlistUsedIDs());
+		for (Integer Iterator = 0; Iterator < 5; Iterator++)
+		{
+			System.out.println(listEmployees.get(Iterator).toString());
+		}
 	}
 }
