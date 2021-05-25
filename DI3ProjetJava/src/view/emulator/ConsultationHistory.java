a308066373ef9dbf41d37d9920b40691fc47778a
RMICH Mohammed <RMICH Mohammed@DESKTOP-40B52IH.mshome.net>
2021-05-25 10:14:05 +0200

    Interface

--- DI3ProjetJava/src/view/emulator/ConsultationHistory.java
+++ DI3ProjetJava/src/view/emulator/ConsultationHistory.java
@@ -1,16 +1,26 @@
 package view.emulator;
 
+import java.awt.Color;
+//import java.awt.Dimension;
+//import java.awt.Toolkit;
 import java.util.concurrent.CopyOnWriteArrayList;
+import javax.swing.*;
+
+
 import model.shared.CheckInOut;
 
-public class ConsultationHistory 
+
+public class ConsultationHistory extends JFrame 
 {
 	/* ================================================================= */
 	/************************* CLASS ATTRIBUTES **************************/
 	/*********************************************************************/
-
+	private static final long serialVersionUID = 1L;
+	private static final int BUTTON_LOCATION_X = 330;  		// location x 
+	private static final int BUTTON_LOCATION_Y = 208;   	// location y 
+	private static final int BUTTON_SIZE_X = 150;      		// size height
+	private static final int BUTTON_SIZE_Y = 50;       		// size width
 	private CopyOnWriteArrayList<CheckInOut> checks;
-
 	/*********************************************************************/
 	/*********************************************************************/
 	/* ================================================================= */
@@ -19,8 +29,26 @@ public class ConsultationHistory
 	/*************************** CONSTRUCTORS ****************************/
 	/*********************************************************************/
 	public ConsultationHistory() 
-	{
-		setChecks(checks);
+	{		
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		// Size of the window
+		this.setSize(500, 300);
+		
+		// Center the window on the screen
+		this.setLocationRelativeTo(null);
+		
+		JPanel panel = new JPanel();
+		JButton startButton = new JButton("Check in/out");
+        startButton.setBounds(BUTTON_LOCATION_X, BUTTON_LOCATION_Y, BUTTON_SIZE_X, BUTTON_SIZE_Y );
+        this.add(startButton);
+		
+		this.add(panel);
+		this.setTitle("Time Tracker Emulator");
+		//panel.getRootPane().setBackground(Color.YELLOW);
+		this.setVisible(true);
+		
+	
 	}
 
 	/**
@@ -73,4 +101,13 @@ public class ConsultationHistory
 	/*********************************************************************/
 	/*********************************************************************/
 	/* ================================================================= */
+	
+	
+	
+	public static void main(String[] args) 
+	{
+		new ConsultationHistory();
+	}
+	
 }
+
