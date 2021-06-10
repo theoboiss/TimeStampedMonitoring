31e2a12b875e543d173ba210197742d5e16bc460
Mohammed Rmich <mohammed.rmich@etu.univ-tours.fr>
2021-06-10 17:58:23 +0200

    Corrections

--- DI3ProjetJava/src/model/emulator/History.java
+++ DI3ProjetJava/src/model/emulator/History.java
@@ -16,85 +16,33 @@ import java.util.concurrent.CopyOnWriteArrayList;
  *
  */
 public class History implements Serializable {
-	/**
-	 * 
-	 */
+
+	/* ================================================================= */
+	/************************* CLASS ATTRIBUTES **************************/
+	/*********************************************************************/
 	private static final long serialVersionUID = 1L;
 
 	public static enum EventDuringCheck {
 		enter, leave
-	};
+	}
 
+	// All CheckInOut per employee from the first day
+	private static Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>> checksPerEmployee;
+	// All CheckInOut per employee during a day. The table is reset to 0 at the end
+	// of day
+	private static Hashtable<LocalDate, Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>>> checksPerEmployeePerDay;
+	private CheckInOut checkInOut;
+	private LocalDateTime dateTime;
+	private Integer employeeID;
 	private EventDuringCheck eventDuringCheck;
 
-	
-	private Settings settings; // All CheckInOut per employee from the first day
-	private static Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>> checksPerEmployee; // All CheckInOut per employee during a day. The table is reset to 0 at the end of day. 
-	/**
-	 * @return the settings
-	 */
-	public Settings getSettings() {
-		return settings;
-	}
-
-	/**
-	 * @param settings the settings to set
-	 */
-	public void setSettings(Settings settings) {
-		this.settings = settings;
-	}
-
-	/**
-	 * @return the checksPerEmployee
-	 */
-	public static Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>> getChecksPerEmployee() {
-		return checksPerEmployee;
-	}
-
-	/**
-	 * @param checksPerEmployee the checksPerEmployee to set
-	 */
-	public static void setChecksPerEmployee(Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>> checksPerEmployee) {
-		History.checksPerEmployee = checksPerEmployee;
-	}
-
-	/**
-	 * @return the checksPerEmployeePerDay
-	 */
-	public static Hashtable<LocalDate, Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>>> getChecksPerEmployeePerDay() {
-		return checksPerEmployeePerDay;
-	}
-
-	/**
-	 * @param checksPerEmployeePerDay the checksPerEmployeePerDay to set
-	 */
-	public static void setChecksPerEmployeePerDay(
-			Hashtable<LocalDate, Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>>> checksPerEmployeePerDay) {
-		History.checksPerEmployeePerDay = checksPerEmployeePerDay;
-	}
-
-	private static Hashtable<LocalDate, Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>>> checksPerEmployeePerDay;
-	 
-	private Integer employeeID;
-	private LocalDateTime dateTime;
-	private CheckInOut checkInOut;
-
+	/* ================================================================= */
+	/*************************** BUILDERS ********************************/
+	/*********************************************************************/
 	public History() {
-		settings = new Settings();
+
 		checksPerEmployee = new Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>>();
 		checksPerEmployeePerDay = new Hashtable<LocalDate, Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>>>();
-	}
-	
-	/**
-	 * @param employeeID
-	 * @param dateTime
-	 * @param checkInOut
-	 */
-	public History(Integer employeeID, LocalDateTime dateTime, CheckInOut checkInOut) {
-		super();
-		this.employeeID = employeeID;
-		this.dateTime = dateTime;
-		this.checkInOut = checkInOut;
 	}
 
 	/**
@@ -109,71 +57,33 @@ public class History implements Serializable {
 	}
 
 	/**
-	 * @brief Method to create an event when an employee arrives or leaves the
-	 *        company
-	 * @param event
-	 * @param empID
-	 * @param time
-	 * @param check
+	 * @param employeeID
+	 * @param dateTime
+	 * @param checkInOut
 	 */
-	/*public void addToHistory(EventDuringCheck event, Integer empID, LocalDateTime time, CheckInOut check) {
-		eventDuringCheck = event;
-		employeeID = empID;
-		dateTime = time;
-		checkInOut = check;
-	}*/
-	/**
-	 * @brief Delete an event from history
-	 */
-	public void deleteElementFromHistory()
-	{
-		eventDuringCheck = null;
-		employeeID = 0;
-		dateTime = null;
-		checkInOut = null;
-	}
-	
-	public static void addToHistory(CheckInOut check, EmployeeInfo info, LocalDate date)
-	{
-		CopyOnWriteArrayList<CheckInOut> list = new CopyOnWriteArrayList<CheckInOut>();
-		list = checksPerEmployee.get(info);
-		checksPerEmployee.put(info, list);
-		checksPerEmployeePerDay.put(date, checksPerEmployee);
-
-	}
-	
-	public static void addToHistory(CheckInOut check, EmployeeInfo info)
-	{
-		CopyOnWriteArrayList<CheckInOut> list = new CopyOnWriteArrayList<CheckInOut>();
-		list = checksPerEmployee.get(info);
-		checksPerEmployee.put(info, list);
-	}
-	/**
-	 * @return the employeeID
-	 */
-	public Integer getEmployeeID() {
-		return employeeID;
-	}
-
-	/**
-	 * @param employeeID the employeeID to set
-	 */
-	public void setEmployeeID(Integer employeeID) {
+	public History(Integer employeeID, LocalDateTime dateTime, CheckInOut checkInOut) {
+		super();
 		this.employeeID = employeeID;
-	}
-
-	/**
-	 * @return the dateTime
-	 */
-	public LocalDateTime getDateTime() {
-		return dateTime;
-	}
-
-	/**
-	 * @param dateTime the dateTime to set
-	 */
-	public void setDateTime(LocalDateTime dateTime) {
 		this.dateTime = dateTime;
+		this.checkInOut = checkInOut;
+	}
+
+	/* ================================================================= */
+	/*************************** GETTERS/SETTERS *************************/
+	/*********************************************************************/
+	
+	/**
+	 * @return the checksPerEmployee
+	 */
+	public static Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>> getChecksPerEmployee() {
+		return checksPerEmployee;
+	}
+
+	/**
+	 * @return the checksPerEmployeePerDay
+	 */
+	public static Hashtable<LocalDate, Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>>> getChecksPerEmployeePerDay() {
+		return checksPerEmployeePerDay;
 	}
 
 	/**
@@ -184,10 +94,17 @@ public class History implements Serializable {
 	}
 
 	/**
-	 * @param checkInOut the checkInOut to set
+	 * @return the dateTime
 	 */
-	public void setCheckInOut(CheckInOut checkInOut) {
-		this.checkInOut = checkInOut;
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	/**
+	 * @return the employeeID
+	 */
+	public Integer getEmployeeID() {
+		return employeeID;
 	}
 
 	/**
@@ -198,17 +115,88 @@ public class History implements Serializable {
 	}
 
 	/**
+	 * @param checksPerEmployee the checksPerEmployee to set
+	 */
+	public static void setChecksPerEmployee(Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>> checksPerEmployee) {
+		History.checksPerEmployee = checksPerEmployee;
+	}
+
+	/**
+	 * @param checksPerEmployeePerDay the checksPerEmployeePerDay to set
+	 */
+	public static void setChecksPerEmployeePerDay(
+			Hashtable<LocalDate, Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>>> checksPerEmployeePerDay) {
+		History.checksPerEmployeePerDay = checksPerEmployeePerDay;
+	}
+
+	/**
+	 * @param checkInOut the checkInOut to set
+	 */
+	public void setCheckInOut(CheckInOut checkInOut) {
+		this.checkInOut = checkInOut;
+	}
+
+	/**
+	 * @param dateTime the dateTime to set
+	 */
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	/**
+	 * @param employeeID the employeeID to set
+	 */
+	public void setEmployeeID(Integer employeeID) {
+		this.employeeID = employeeID;
+	}
+
+	/**
 	 * @param eventDuringCheck the eventDuringCheck to set
 	 */
 	public void setEventDuringCheck(EventDuringCheck eventDuringCheck) {
 		this.eventDuringCheck = eventDuringCheck;
 	}
 
+	/* ================================================================= */
+	/***************************** METHODS *******************************/
+	/*********************************************************************/
+
+	public static void addToHistory(CheckInOut check, EmployeeInfo info) {
+		CopyOnWriteArrayList<CheckInOut> list = new CopyOnWriteArrayList<CheckInOut>();
+		list = checksPerEmployee.get(info);
+		checksPerEmployee.put(info, list);
+	}
+
+	public static void addToHistory(CheckInOut check, EmployeeInfo info, LocalDate date) {
+		CopyOnWriteArrayList<CheckInOut> list = new CopyOnWriteArrayList<CheckInOut>();
+		list = checksPerEmployee.get(info);
+		checksPerEmployee.put(info, list);
+		checksPerEmployeePerDay.put(date, checksPerEmployee);
+
+	}
+
 	/**
-	 * @return the serialversionuid
+	 * @brief Method to create an event when an employee arrives or leaves the
+	 *        company
+	 * @param event
+	 * @param empID
+	 * @param time
+	 * @param check
 	 */
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	/*
+	 * public void addToHistory(EventDuringCheck event, Integer empID, LocalDateTime
+	 * time, CheckInOut check) { eventDuringCheck = event; employeeID = empID;
+	 * dateTime = time; checkInOut = check; }
+	 */
+	
+	/**
+	 * @brief Delete an event from history
+	 */
+	public void deleteElementFromHistory() {
+		eventDuringCheck = null;
+		employeeID = 0;
+		dateTime = null;
+		checkInOut = null;
 	}
 
 	/**
@@ -230,121 +218,5 @@ public class History implements Serializable {
 		}
 		return event;
 	}
-
-	/* ================================================================= */
-	/************************* CLASS ATTRIBUTES **************************/
-	/*********************************************************************/
-
-	/*
-	 * private Settings settings; // All CheckInOut per employee from the first day
-	 * private Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>>
-	 * checksPerEmployee; // All CheckInOut per employee during a day. The table is
-	 * reset to 0 at the end of day. private Hashtable<LocalDate,
-	 * Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>>>
-	 * checksPerEmployeePerDay;
-	 */
-	/*********************************************************************/
-	/*********************************************************************/
-	/* ================================================================= */
-
-	/* ================================================================= */
-	/*************************** CONSTRUCTORS ****************************/
-	/*********************************************************************/
-
-	
-	
-
-	 
-
-	/**
-	 * @param settings
-	 * @param checksPerEmployee
-	 * @param checksPerEmployeePerDay
-	 */
-	/*
-	 * public History(Settings settings, Hashtable<EmployeeInfo,
-	 * CopyOnWriteArrayList<CheckInOut>> checksPerEmployee, Hashtable<LocalTime,
-	 * Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>>>
-	 * checksPerEmployeePerDay) {
-	 * 
-	 * super(); this.settings = settings; this.checksPerEmployee =
-	 * checksPerEmployee; this.checksPerEmployeePerDay = checksPerEmployeePerDay; }
-	 */
-	/*********************************************************************/
-	/*********************************************************************/
-	/* ================================================================= */
-
-	/* ================================================================= */
-	/********************** GETTERS AND SETTERS **************************/
-	/*********************************************************************/
-
-	/**
-	 * @return the settings
-	 */
-	/*
-	 * public Settings getSettings() { return settings; }
-	 */
-
-	/**
-	 * @param settings the settings to set
-	 */
-	/*
-	 * public void setSettings(Settings settings) { this.settings = settings; }
-	 */
-
-	/**
-	 * @return the checksPerEmployee
-	 */
-
-	/*
-	 * public Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>>
-	 * getChecksPerEmployee() {
-	 * 
-	 * return checksPerEmployee; }
-	 */
-
-	/**
-	 * @param checksPerEmployee the checksPerEmployee to set
-	 */
-
-	/*
-	 * public void setChecksPerEmployee(Hashtable<EmployeeInfo,
-	 * CopyOnWriteArrayList<CheckInOut>> checksPerEmployee) {
-	 * 
-	 * this.checksPerEmployee = checksPerEmployee; }
-	 */
-
-	/**
-	 * @return the checksPerEmployeePerDay
-	 */
-
-	/*
-	 * public Hashtable<LocalTime, Hashtable<EmployeeInfo,
-	 * CopyOnWriteArrayList<CheckInOut>>> getChecksPerEmployeePerDay() {
-	 * 
-	 * return checksPerEmployeePerDay; }
-	 */
-
-	/**
-	 * @param checksPerEmployeePerDay the checksPerEmployeePerDay to set
-	 */
-	/*
-	 * public void setChecksPerEmployeePerDay(Hashtable<LocalTime,
-	 * Hashtable<EmployeeInfo, CopyOnWriteArrayList<CheckInOut>>>
-	 * checksPerEmployeePerDay) {
-	 * 
-	 * this.checksPerEmployeePerDay = checksPerEmployeePerDay; }
-	 */
-	/*********************************************************************/
-	/*********************************************************************/
-	/* ================================================================= */
-
-	/* ================================================================= */
-	/***************************** METHODS *******************************/
-	/*********************************************************************/
-
-	/*********************************************************************/
-	/*********************************************************************/
-	/* ================================================================= */
 
 }
