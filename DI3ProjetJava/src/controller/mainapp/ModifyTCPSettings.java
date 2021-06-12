f62399ca2cd1df0eb2e03bfe6f3fdac3a0c06e19
Mohammed Rmich <mohammed.rmich@etu.univ-tours.fr>
2021-06-12 17:38:04 +0200

    tcp settings

--- DI3ProjetJava/src/controller/mainapp/ModifyTCPSettings.java
+++ DI3ProjetJava/src/controller/mainapp/ModifyTCPSettings.java
@@ -14,42 +14,38 @@ import view.mainapp.ViewSettings;
  */
 public class ModifyTCPSettings implements Serializable {
 
+	/* ================================================================= */
+	/*************************** ATTRIBUTES ******************************/
+	/*********************************************************************/
+
 	private static final long serialVersionUID = -8826014027937834304L;
 	private String regexPattern;
 	private TCPMainAppSettings settings;
-	
-	
-	public ModifyTCPSettings(TCPMainAppSettings settings2)
-	{
-		this.settings = settings2;	
+
+	/* ================================================================= */
+	/**************************** BUILDERS *******************************/
+	/*********************************************************************/
+
+	/**
+	 * @brief Default constructor
+	 */
+	public ModifyTCPSettings() {
+		regexPattern = new String();
 	}
 
 	/**
-	 * @brief Method to modify port number and IP address in the main application
-	 * @param request
-	 * @return
+	 * @brief One argument copy constructor
+	 * @param settings2
 	 */
-	public String ModifyConnectionSettings(HashMap<String, JTextField> request) throws UnknownHostException {
-
-		// extract information from the request
-		String newPortNumberServer = request.get("portnumber_server").getText().split(getRegexPattern())[0];
-		String newIPAddressServer = request.get("ipaddress_server").getText().split(getRegexPattern())[0];
-		
-		// Converting strings to the correct form 
-		Integer portNumberServerToSet = Integer.parseInt(newPortNumberServer);
-		byte[] IPAddressServerToSet = newIPAddressServer.getBytes();
-
-		// Changing elements
-		
-		settings.setIPaddressServer(InetAddress.getByAddress(IPAddressServerToSet)); // to change when it will be possible to serialize
-		settings.setNumPortServer(portNumberServerToSet); // to change when it will be possible to serialize
-		//setIPaddressClient(InetAddress.getByAddress(ipAddrC));
-		//setNumPortClient(4045);
-		
-		
-		return "Port number and IP address successfully changed.";
+	public ModifyTCPSettings(TCPMainAppSettings settings2) {
+		this.settings = settings2;
 	}
 
+	/* ================================================================= */
+	/**************************** GETS/SETS ******************************/
+	/*********************************************************************/
+
+	/*************************** regexPattern ****************************/
 	/**
 	 * @return the regexPattern
 	 */
@@ -62,5 +58,52 @@ public class ModifyTCPSettings implements Serializable {
 	 */
 	public void setRegexPattern(String regexPattern) {
 		this.regexPattern = regexPattern;
+	}
+
+	/***************************** settings ******************************/
+	/**
+	 * @return the settings
+	 */
+	public TCPMainAppSettings getSettings() {
+		return settings;
+	}
+
+	/**
+	 * @param settings the settings to set
+	 */
+	public void setSettings(TCPMainAppSettings settings) {
+		this.settings = settings;
+	}
+
+	/* ================================================================= */
+	/***************************** METHODS *******************************/
+	/*********************************************************************/
+
+	/**
+	 * @brief Method to modify port number and IP address in the main application
+	 * @param request
+	 * @return
+	 */
+	public String ModifyConnectionSettings(HashMap<String, JTextField> request) throws UnknownHostException {
+
+		// extract information from the request
+		String newPortNumberServer = request.get("portnumber_server").getText().split(getRegexPattern())[0];
+		String newIPAddressServer = request.get("ipaddress_server").getText().split(getRegexPattern())[0];
+		String newPortNumberClient = request.get("portnumber_client").getText().split(getRegexPattern())[0];
+		String newIPAddressClient = request.get("ipaddress_client").getText().split(getRegexPattern())[0];
+
+		// Converting strings to the correct form
+		Integer portNumberServerToSet = Integer.parseInt(newPortNumberServer);
+		byte[] IPAddressServerToSet = newIPAddressServer.getBytes();
+		Integer portNumberClientToSet = Integer.parseInt(newPortNumberClient);
+		byte[] IPAddressClientToSet = newIPAddressClient.getBytes();
+
+		// Changing elements
+		settings.setNumPortServer(portNumberServerToSet);
+		settings.setIPaddressServer(InetAddress.getByAddress(IPAddressServerToSet));
+		settings.setNumPortServer(portNumberClientToSet);
+		settings.setIPaddressServer(InetAddress.getByAddress(IPAddressClientToSet));
+
+		return "Port number and IP address successfully changed.";
 	}
 }
