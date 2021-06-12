50cd6748eb8aacaf353d8b4af0c9c13024593c42
Mohammed Rmich <mohammed.rmich@etu.univ-tours.fr>
2021-06-12 17:25:15 +0200

    client added

--- DI3ProjetJava/src/view/mainapp/ViewSettings.java
+++ DI3ProjetJava/src/view/mainapp/ViewSettings.java
@@ -4,18 +4,14 @@ import java.awt.event.ActionEvent;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-
-import controller.mainapp.BrowserMainapp;
-import controller.mainapp.MainappSettings;
 import controller.mainapp.ModifyTCPSettings;
 import controller.mainapp.TCPMainAppSettings;
 
 /**
- * @brief Class to manage and modify connection settings (port number and IP
- *        address for the main application).
+ * @brief Class to show connection settings (port number and IP
+ *        address for the main application) in the interface.
  *
  */
 public class ViewSettings extends ViewModel {
@@ -24,12 +20,16 @@ public class ViewSettings extends ViewModel {
 	/**************************** ATTRIBUTES *****************************/
 	/*********************************************************************/
 
-	private JTextField fieldPortNumber;
-	private JTextField fieldIPAddress;
+	private JTextField fieldPortNumberServer;
+	private JTextField fieldIPAddressServer;
+	private JTextField fieldPortNumberClient;
+	private JTextField fieldIPAddressClient;
 
-	private JLabel labelPortNumber;
-	private JLabel labelIPAddress;
-	
+	private JLabel labelPortNumberServer;
+	private JLabel labelIPAddressServer;
+	private JLabel labelPortNumberClient;
+	private JLabel labelIPAddressClient;
+
 	private TCPMainAppSettings settings;
 
 	/* ================================================================= */
@@ -40,80 +40,155 @@ public class ViewSettings extends ViewModel {
 	 * @brief Default constructor
 	 */
 	public ViewSettings() {
-		setArraySize(2);
+		setArraySize(4);
 		initializeAttributes();
 		buildContentPanel();
 	}
 
-	public ViewSettings(TCPMainAppSettings settings)
-	{
+	public ViewSettings(TCPMainAppSettings settings) {
 		this.settings = settings;
-		setArraySize(2);
+		setArraySize(4);
 		initializeAttributes();
 		buildContentPanel();
 	}
+
 	/* ================================================================= */
 	/*************************** SETS/GETS *******************************/
 	/*********************************************************************/
 
-	/************************ fieldPortNumber ****************************/
+	/************************ fieldPortNumberServer **********************/
 	/**
-	 * @return the fieldPortNumber
+	 * @return the fieldPortNumberServer
 	 */
-	public JTextField getFieldPortNumber() {
-		return fieldPortNumber;
+	public JTextField getFieldPortNumberServer() {
+		return fieldPortNumberServer;
 	}
 
 	/**
-	 * @param fieldPortNumber the fieldPortNumber to set
+	 * @param fieldPortNumberServer the fieldPortNumberServer to set
 	 */
-	public void setFieldPortNumber(JTextField fieldPortNumber) {
-		this.fieldPortNumber = fieldPortNumber;
+	public void setFieldPortNumberServer(JTextField fieldPortNumberServer) {
+		this.fieldPortNumberServer = fieldPortNumberServer;
 	}
 
-	/************************* fieldIPAddress ****************************/
+	/************************ fieldIPAddressServer ***********************/
 	/**
-	 * @return the fieldIPAddress
+	 * @return the fieldIPAddressServer
 	 */
-	public JTextField getFieldIPAddress() {
-		return fieldIPAddress;
-	}
-
-	/**
-	 * @param fieldIPAddress the fieldIPAddress to set
-	 */
-	public void setFieldIPAddress(JTextField fieldIPAddress) {
-		this.fieldIPAddress = fieldIPAddress;
-	}
-
-	/************************* labelPortNumber ***************************/
-	/**
-	 * @return the labelPortNumber
-	 */
-	public JLabel getLabelPortNumber() {
-		return labelPortNumber;
+	public JTextField getFieldIPAddressServer() {
+		return fieldIPAddressServer;
 	}
 
 	/**
-	 * @param labelPortNumber the labelPortNumber to set
+	 * @param fieldIPAddressServer the fieldIPAddressServer to set
 	 */
-	public void setLabelPortNumber(JLabel labelPortNumber) {
-		this.labelPortNumber = labelPortNumber;
+	public void setFieldIPAddressServer(JTextField fieldIPAddressServer) {
+		this.fieldIPAddressServer = fieldIPAddressServer;
+	}
+	
+	/*********************** fieldPortNumberClient ***********************/
+	/**
+	 * @return the fieldPortNumberClient
+	 */
+	public JTextField getFieldPortNumberClient() {
+		return fieldPortNumberClient;
 	}
 
-	/************************** labelIPAddress ***************************/
 	/**
-	 * @return the labelIPAddress
+	 * @param fieldPortNumberClient the fieldPortNumberClient to set
 	 */
-	public JLabel getLabelIPAddress() {
-		return labelIPAddress;
+	public void setFieldPortNumberClient(JTextField fieldPortNumberClient) {
+		this.fieldPortNumberClient = fieldPortNumberClient;
+	}
+	
+	/*********************** fieldIPAddressClient ************************/
+	/**
+	 * @return the fieldIPAddressClient
+	 */
+	public JTextField getFieldIPAddressClient() {
+		return fieldIPAddressClient;
 	}
 
 	/**
-	 * @param labelIPAddress the labelIPAddress to set
+	 * @param fieldIPAddressClient the fieldIPAddressClient to set
 	 */
-	public void setLabelIPAddress(JLabel labelIPAddress) {
-		this.labelIPAddress = labelIPAddress;
+	public void setFieldIPAddressClient(JTextField fieldIPAddressClient) {
+		this.fieldIPAddressClient = fieldIPAddressClient;
+	}
+
+	/*********************** labelPortNumberServer ***********************/
+	/**
+	 * @return the labelPortNumberServer
+	 */
+	public JLabel getLabelPortNumberServer() {
+		return labelPortNumberServer;
+	}
+
+	/**
+	 * @param labelPortNumberServer the labelPortNumberServer to set
+	 */
+	public void setLabelPortNumberServer(JLabel labelPortNumberServer) {
+		this.labelPortNumberServer = labelPortNumberServer;
+	}
+	
+	/************************ labelIPAddressServer ***********************/
+	/**
+	 * @return the labelIPAddressServer
+	 */
+	public JLabel getLabelIPAddressServer() {
+		return labelIPAddressServer;
+	}
+
+	/**
+	 * @param labelIPAddressServer the labelIPAddressServer to set
+	 */
+	public void setLabelIPAddressServer(JLabel labelIPAddressServer) {
+		this.labelIPAddressServer = labelIPAddressServer;
+	}
+
+	/********************** labelPortNumberClient ************************/
+	/**
+	 * @return the labelPortNumberClient
+	 */
+	public JLabel getLabelPortNumberClient() {
+		return labelPortNumberClient;
+	}
+
+	/**
+	 * @param labelPortNumberClient the labelPortNumberClient to set
+	 */
+	public void setLabelPortNumberClient(JLabel labelPortNumberClient) {
+		this.labelPortNumberClient = labelPortNumberClient;
+	}
+
+	/********************** labelIPAddressClient *************************/
+	/**
+	 * @return the labelIPAddressClient
+	 */
+	public JLabel getLabelIPAddressClient() {
+		return labelIPAddressClient;
+	}
+
+	/**
+	 * @param labelIPAddressClient the labelIPAddressClient to set
+	 */
+	public void setLabelIPAddressClient(JLabel labelIPAddressClient) {
+		this.labelIPAddressClient = labelIPAddressClient;
+	}
+	
+	/**************************** settings *******************************/
+	/**
+	 * @return the settings
+	 */
+	public TCPMainAppSettings getSettings() {
+		return settings;
+	}
+	
+	/**
+	 * @param settings the settings to set
+	 */
+	public void setSettings(TCPMainAppSettings settings) {
+		this.settings = settings;
 	}
 
 	/* ================================================================= */
@@ -123,16 +198,28 @@ public class ViewSettings extends ViewModel {
 	@Override
 	protected void initializeAttributes() {
 		labelArray = new ArrayList<JLabel>();
-		labelPortNumber = new JLabel("Port Number ");
-		labelIPAddress = new JLabel("IP Address ");
-		labelArray.add(labelPortNumber);
-		labelArray.add(labelIPAddress);
-		textFieldArray = new ArrayList<JTextField>();
-		fieldPortNumber = new JTextField();
-		fieldIPAddress = new JTextField();
-		textFieldArray.add(fieldPortNumber);
-		textFieldArray.add(fieldIPAddress);
 
+		labelPortNumberServer = new JLabel("Server Port Number ");
+		labelIPAddressServer = new JLabel("Server IP Address ");
+		labelPortNumberClient = new JLabel("Client Port Number ");
+		labelIPAddressClient = new JLabel("Client IP Address ");
+
+		labelArray.add(labelPortNumberServer);
+		labelArray.add(labelIPAddressServer);
+		labelArray.add(labelPortNumberClient);
+		labelArray.add(labelIPAddressClient);
+
+		textFieldArray = new ArrayList<JTextField>();
+
+		fieldPortNumberServer = new JTextField();
+		fieldIPAddressServer = new JTextField();
+		fieldPortNumberClient = new JTextField();
+		fieldIPAddressClient = new JTextField();
+
+		textFieldArray.add(fieldPortNumberServer);
+		textFieldArray.add(fieldIPAddressServer);
+		textFieldArray.add(fieldPortNumberClient);
+		textFieldArray.add(fieldIPAddressClient);
 	}
 
 	@Override
@@ -144,16 +231,17 @@ public class ViewSettings extends ViewModel {
 			submitMap = new HashMap<String, JTextField>();
 			submitMap.put("portnumber_server", textFieldArray.get(0));
 			submitMap.put("ipaddress_server", textFieldArray.get(1));
+			submitMap.put("portnumber_client", textFieldArray.get(2));
+			submitMap.put("ipaddress_client", textFieldArray.get(3));
 			System.out.println(submitMap);
 
 			ModifyTCPSettings settingsToModify = new ModifyTCPSettings(settings);
 			try {
 				settingsToModify.ModifyConnectionSettings(submitMap);
 			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
 				System.out.println(e.getMessage());
 			}
-			
+
 		}
 
 	}
