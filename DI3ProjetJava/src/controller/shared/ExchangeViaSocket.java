27442f4dd4ab8d28d328c8da471ebcaec60fb37e
Mohammed Rmich <mohammed.rmich@etu.univ-tours.fr>
2021-06-13 23:42:51 +0200

    5th package

--- DI3ProjetJava/src/controller/shared/ExchangeViaSocket.java
+++ DI3ProjetJava/src/controller/shared/ExchangeViaSocket.java
@@ -3,6 +3,10 @@ package controller.shared;
 import java.io.IOException;
 import java.net.Socket;
 
+/**
+ * @brief Class for communication using sockets
+ *
+ */
 public class ExchangeViaSocket extends SerializationProcess {
 
 	/*********************************************************************/
@@ -11,11 +15,22 @@ public class ExchangeViaSocket extends SerializationProcess {
 
 	/******************************* send ********************************/
 
+	/**
+	 * @param thisMachine
+	 * @param dataToSave
+	 * @throws IOException
+	 */
 	public void send(Socket thisMachine, Object dataToSave) throws IOException {
 		initialize(thisMachine.getOutputStream());
 		insert(dataToSave);
 	}
 
+	/**
+	 * @param thisMachine
+	 * @param dataToSave
+	 * @param streamStatus
+	 * @throws IOException
+	 */
 	public void send(Socket thisMachine, Object dataToSave, int streamStatus) throws IOException {
 		if (streamStatus > 0)
 			initialize(thisMachine.getOutputStream());
@@ -26,11 +41,24 @@ public class ExchangeViaSocket extends SerializationProcess {
 
 	/****************************** receive ******************************/
 
+	/**
+	 * @param remoteMachine
+	 * @return
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
 	public Object receive(Socket remoteMachine) throws ClassNotFoundException, IOException {
 		initialize(remoteMachine.getInputStream());
 		return extract();
 	}
 
+	/**
+	 * @param remoteMachine
+	 * @param streamStatus
+	 * @return
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
 	public Object receive(Socket remoteMachine, int streamStatus) throws ClassNotFoundException, IOException {
 		if (streamStatus > 0)
 			initialize(remoteMachine.getInputStream());
